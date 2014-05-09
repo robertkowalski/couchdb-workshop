6570969f38f9f1196b8ca918e7b63612478e311a
Robert Kowalski <rok@kowalski.gd>
2014-05-10 00:44:33 +0200

    init

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('workshopCouchApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
