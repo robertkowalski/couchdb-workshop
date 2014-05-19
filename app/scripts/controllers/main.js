9609ae513df58c0831d260ff2b173c14ba00e709
Robert Kowalski <rok@kowalski.gd>
2014-05-19 19:26:54 +0200

    Fetch the first view
    Fetch the items on start of the app and show the first item

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,6 +1,13 @@
 'use strict';
 
 angular.module('workshopCouchApp')
-  .controller('MainCtrl', function ($scope, appSettings) {
-    console.log(appSettings);
+  .controller('MainCtrl', function ($scope, $http, appSettings) {
+
+    function getItems () {
+      $http.get(appSettings.db + '/_design/expenses/_view/byName')
+        .success(function (data) {
+          $scope.items = data.rows;
+        });
+    }
+    getItems();
   });
--- test/spec/controllers/main.js
+++ test/spec/controllers/main.js
@@ -5,18 +5,41 @@ describe('Controller: MainCtrl', function () {
   // load the controller's module
   beforeEach(module('workshopCouchApp'));
 
-  var MainCtrl,
-    scope;
+  var $httpBackend,
+      $rootScope,
+      createController,
+      appSettings;
 
-  // Initialize the controller and a mock scope
-  beforeEach(inject(function ($controller, $rootScope) {
-    scope = $rootScope.$new();
-    MainCtrl = $controller('MainCtrl', {
-      $scope: scope
-    });
+  beforeEach(inject(function ($injector) {
+
+    appSettings = $injector.get('appSettings');
+
+    $httpBackend = $injector.get('$httpBackend');
+
+    var items = {total_rows: 2, offset: 0, rows: [
+      {id: 'd19027710152f7600216d0b84e009ab2', 'key': 'Angular Book',
+        'value': 29.989999999999998437},
+      {id: 'd19027710152f7600216d0b84e00a887', 'key': 'Node.js Hosting',
+        'value': 3.3333333333333333333}
+    ]};
+
+    $httpBackend.when('GET', appSettings.couchRoot + appSettings.itemsView)
+      .respond(items);
+
+    $rootScope = $injector.get('$rootScope');
+
+    var $controller = $injector.get('$controller');
+    createController = function() {
+      return $controller('MainCtrl', {'$scope' : $rootScope });
+    };
+
   }));
 
-  it('should attach a list of awesomeThings to the scope', function () {
-    assert.equal(scope.awesomeThings.length, 3);
+  it('should fetch items', function() {
+    $httpBackend.expectGET(appSettings.couchRoot + appSettings.itemsView);
+    createController();
+    $httpBackend.flush();
+    assert.equal($rootScope.items.length, 2);
   });
+
 });
