a44384211c152e059c2c56b18e708a4db29268d6
Robert Kowalski <rok@kowalski.gd>
2014-07-28 18:42:38 +0200

    add validate function to design document

--- couchdb/views.js
+++ couchdb/views.js
@@ -1,5 +1,22 @@
 var ddoc = {
-  _id: '_design/expenses', views: {}, lists: {}, shows: {}
+  _id: '_design/expenses',
+  views: {},
+  lists: {},
+  shows: {},
+  validate_doc_update: function (newDoc, oldDoc, userCtx, secObj) {
+    if (newDoc._deleted === true) {
+      return;
+    }
+    if (!newDoc.name) {
+      throw({forbidden: 'Document must have an item name.'});
+    }
+    if (!newDoc.price) {
+      throw({forbidden: 'Document must have a price.'});
+    }
+    if (!/\d+\.\d\d/.test(newDoc.price)) {
+      throw({forbidden: 'Price must be a number and have two decimal places after a dot.'});
+    }
+  }
 };
 
 // _design/expenses/_view/byName
